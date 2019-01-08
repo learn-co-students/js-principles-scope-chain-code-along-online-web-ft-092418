fdce2d29f5ec688960b9c50fdeb4a329fbacf467
Doug Fitzgerald <dfitzge1@conncoll.edu>
2019-01-08 12:49:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 /* Your code goes here */
+var customerName = "bob";
+var bestCustomer;
+const leastFavoriteCustomer = "Homer";
+
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Marge";
+}
+
+function attemptTwoFavoriteCustomers() {
+  const favoriteCustomer = "Bart";
+  const favoriteCustomer = "Milhouse";
+}
